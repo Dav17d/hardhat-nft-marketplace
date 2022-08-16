dbbff791a27f2c9d8e6a65212555a95c821389e6
Dav17d <75180393+Dav17d@users.noreply.github.com>
2022-08-16 18:51:27 +0500

    Updated

--- deploy/99-update-front-end.js
+++ deploy/99-update-front-end.js
@@ -2,12 +2,28 @@ const { ethers, network, getChainId } = require("hardhat")
 const fs = require("fs")
 
 const frontEndContractsFile = "../nextjs-nft-marketplace-moralis/constants/networkMapping.json"
+const frontEndAbiFile = "../nextjs-nft-marketplace-moralis/constants/"
 
 module.exports = async function () {
     if (process.env.UPDATE_FRONT_END) {
         console.log("Updating front end...")
         await updateContractAddresses()
+        await updateAbi()
     }
+}
+
+async function updateAbi() {
+    const nftMarkteplace = await ethers.getContract("NftMarketplace")
+    fs.writeFileSync(
+        `${frontEndAbiFile}NftMarketplace.json`,
+        nftMarkteplace.interface.format(ethers.utils.FormatTypes.json)
+    )
+
+    const basicNft = await ethers.getContract("BasicNft")
+    fs.writeFileSync(
+        `${frontEndAbiFile}BasicNft.json`,
+        basicNft.interface.format(ethers.utils.FormatTypes.json)
+    )
 }
 
 async function updateContractAddresses() {
